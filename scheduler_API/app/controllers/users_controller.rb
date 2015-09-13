dad2f905cad081490298994873de0238b95b3616
Erik Regla <jvarred@gmail.com>
2015-09-13 20:47:11 -0300

    Updated POST and GET methods

--- scheduler_API/app/controllers/users_controller.rb
+++ scheduler_API/app/controllers/users_controller.rb
@@ -1,9 +1,39 @@
 class UsersController < ApplicationController
+  protect_from_forgery with: :null_session
   def index
-    respond_to do |format|
-      format.html
-      format.json {render json:User.all}
-      format.xml {render xml:User.all}
+    if params[:id]
+      begin
+        target = User.find(params[:id])
+        respond_to do |format|
+          format.json {render json:User.find(params[:id])}
+          format.xml {render xml:User.find(params[:id])}
+        end
+      rescue Exception
+        render :text => ""
+      end
+    else
+        respond_to do |format|
+          format.json {render json:User.all}
+          format.xml {render xml:User.all}
+        end
     end
   end
+
+  def create
+    if params[:method] == "delete"
+      result = User.delete_all username: params[:username]
+      render :text => "Deleted #{result} records."
+    elsif params[:method] == "create"
+      result = User.create({username: params[:username], password: params[:password], iid: params[:iid]})
+      if result
+        render :text => "OK"
+      else
+        render :text => "FAIL"
+      end
+    else
+     render :text => "FAIL"
+   end
+  end
+
+
 end
